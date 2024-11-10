b833e490523bd8f22667783f7a42de14e3be3d4f
Serwilaaat <g_ui_g@mail.ru>
2024-11-10 16:41:30 +0700

    sorted_1

--- sortedNazimov/Program.cs
+++ sortedNazimov/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 class Program
 {
@@ -15,6 +16,8 @@ class Program
             QuickSort(arr, pivotIndex + 1, high);
         }
     }
+
+
 
     // Разбиение массива и возвращение индекса опорного элемента
     private static int Partition(int[] arr, int low, int high)
@@ -37,6 +40,8 @@ class Program
         return i + 1;
     }
 
+
+
     // Функция для обмена двух элементов
     private static void Swap(ref int a, ref int b)
     {
@@ -45,31 +50,7 @@ class Program
         b = temp;
     }
 
-    // Главная функция для генерации массива и сортировки
-    public static void Main()
-    {
-        // Размер массива
-        int n = 100000;
-        Random random = new Random();
 
-        // Генерация массива случайных чисел
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            arr[i] = random.Next(1, 100000); // Случайные числа от 1 до 99
-        }
-
-        // Вывод исходного массива
-        Console.WriteLine("Исходный массив:");
-        PrintArray(arr);
-
-        // Сортировка массива с использованием быстрой сортировки
-        QuickSort(arr, 0, arr.Length - 1);
-
-        // Вывод отсортированного массива
-        Console.WriteLine("\nОтсортированный массив:");
-        PrintArray(arr);
-    }
 
     // Функция для вывода массива
     private static void PrintArray(int[] arr)
@@ -79,6 +60,63 @@ class Program
             Console.Write(item + " ");
         }
         Console.WriteLine();
+
     }
+
+
+
+    private static int[] GenerateRandomArray(int size)
+    {
+        
+        Random random = new Random();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = random.Next(1, 100);
+        }
+        return arr;
+    }
+
+
+
+    // Главная функция для генерации массива и сортировки
+    public static void Main()
+    {
+        int[] sizes = {1000, 10000, 100000 };
+
+        Console.WriteLine("\n --- Время алгоритма быстрой сортировки --- ");
+        foreach (int size in sizes)
+        {
+
+            int[] arr = GenerateRandomArray(size);
+
+            // запуск таймера
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Сортировка массива с использованием быстрой сортировки
+            QuickSort(arr, 0, arr.Length - 1);
+
+            // остановка таймера
+            sw.Stop();
+
+
+
+
+            double elapsedSeconds = sw.ElapsedMilliseconds / 1000.0;
+            Console.WriteLine($"\nВремя сортировки {size} чисел: {elapsedSeconds:F4} секунд.");
+
+        }
+        
+        
+
+        
+    }
+
+
+
+    
 }
 
